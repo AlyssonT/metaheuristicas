81feb241b0d054f68bdc62431d2e7741edbff8fa
AlyssonT <73255963+AlyssonT@users.noreply.github.com>
2023-05-25 07:45:12 -0300

    Update trab2.rs

--- trab2.rs
+++ trab2.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::*;
 use std::env;
 use rand::Rng;
+use rand::seq::SliceRandom;
+use std::time::Instant;
 
 fn read_file(file_name: &str) -> String {
     let mut file = File::open(file_name)
@@ -238,7 +240,6 @@ impl Instance {
                 better_option = vec![];
                 eval_first = self.evaluate(&mut solution);
                 eval_better_option = i32::MAX;
-                println!("nao eh que deu, meu");
             } else {
                 return solution;
             }
@@ -262,7 +263,7 @@ impl Instance {
                     .collect();
             while !current_distances.is_empty() {
                 current_distances.sort_by(|a,b| a.1.cmp(b.1));
-                choice = rng.gen_range(0..=((current_distances.len() as f64 * 0.1).floor() as usize));
+                choice = rng.gen_range(0..=((current_distances.len() as f64 * 0.2).floor() as usize));
                 let next_city = current_distances[choice].0;
                 visited[next_city] = true;
                 solution.push(next_city);
@@ -274,9 +275,10 @@ impl Instance {
             }
 
             let mut new_solution = self.local_search(&solution);
-            if eval_best_solution > self.evaluate(&mut new_solution) {
+            let new_eval = self.evaluate(&mut new_solution);
+            if eval_best_solution > new_eval {
                 best_solution = new_solution;
-                eval_best_solution = self.evaluate(&mut best_solution);
+                eval_best_solution = new_eval;
             }
             current = rng.gen_range(0..self.cities.len());
             solution.clear();
@@ -288,22 +290,20 @@ impl Instance {
         best_solution
     }
 
-    fn sa(&self) -> Vec<usize> {
+    fn sa(&self, init: &Vec<usize>, temp: f64, alfa: f64, freeze: f64, max_iter: usize) -> Vec<usize> {
         let mut rng = rand::thread_rng();
-        let mut solution = self.greedy();
+        let mut solution = init.clone();
         let mut eval_solution: i32;
         let mut best_solution = solution.clone();
         let mut eval_best: i32 = self.evaluate(&mut best_solution);
         let mut neighbor: Vec<usize>;
         let mut eval_neighbor: i32;
-        let mut temperature = 1000.0;
-        let alfa = 0.999;
-        let freeze = 0.01;
+        let mut temperature = temp;
         let mut delta: i32;
         while temperature > freeze {
             
             let mut i: usize; let mut j: usize;
-            for _ in 0..1000 {
+            for _ in 0..max_iter {
                 eval_solution = self.evaluate(&mut solution);
                 i = rng.gen_range(0..solution.len());
                 j = rng.gen_range(0..solution.len());
@@ -324,7 +324,6 @@ impl Instance {
                 } else {
                     let x = rng.gen_range(0.0..=1.0);
                     let e: f64 = 2.7182818284590452353602874713527;
-                    //println!("{}", e.powf((-delta as f64) / temperature));
                     if x < (e.powf((-delta as f64) / temperature)) {
                         solution = neighbor;
                     } 
@@ -334,30 +333,78 @@ impl Instance {
         }
         self.local_search(&best_solution)
     }
+
+    fn ils(&self, init: &Vec<usize>) -> Vec<usize> {
+        let mut rng = rand::thread_rng();
+        let mut i: usize;
+        let mut j: usize;
+        let len = init.len();
+        let pert_strength = (len as f64 * 0.02).ceil() as i32;
+        let mut solution = init.clone();
+        let mut eval_solution: i32;
+        let mut best_solution = solution.clone();
+        let mut eval_best_solution = self.evaluate(&mut best_solution);
+
+        for _ in 0..10000 {
+            solution = self.local_search(&solution);
+            eval_solution = self.evaluate(&mut solution);
+
+            if eval_best_solution > eval_solution {
+                best_solution = solution.clone();
+                eval_best_solution = eval_solution;
+            }
+            
+            for _ in 0..pert_strength {
+                i = rng.gen_range(0..len);
+                j = rng.gen_range(0..len);
+                while i == j {
+                    j = rng.gen_range(0..len);
+                }
+                (solution[i], solution[j]) = (solution[j], solution[i]);
+            }
+        }
+        best_solution
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut rng = rand::thread_rng();
 
-    if args.len() < 2 { 
-        println!("Nenhum arquivo especificado");
+    if args.len() < 3 { 
+        println!("Arquivo ou metodo nao especificados");
         return;    
     }
     let mut instance = Instance::new();
-    let tspp_file_name = args[1].as_str();
+    let tspp_file_name = args[2].as_str();
     let matrix_file_name = match args.len() {
-        3..=usize::MAX => Some(args[2].as_str()),
+        4..=usize::MAX => Some(args[3].as_str()),
         _ => None,
     };
 
     instance.set_data(tspp_file_name, matrix_file_name);
 
-    // let mut solution = instance.grasp();
-    // println!("GRASP:");
-    // println!("{:?}", solution);
-    // println!("{:?}", instance.evaluate(&mut solution));
-    println!("\nSA:");
-    let mut sa_solution = instance.sa();
-    println!("{:?}", sa_solution);
-    println!("{}", instance.evaluate(&mut sa_solution));
+    let mut solution: Vec<usize>;
+    let mut random_solution = instance.sequential();
+    random_solution.shuffle(&mut rng);
+    let start = Instant::now();
+    match args[1].to_lowercase().as_str() {
+        "grasp" => {
+            println!("GRASP:");
+            solution = instance.grasp();
+        },
+        "sa" => {
+            println!("Simulated Annealing:");
+            solution = instance.sa(&random_solution, 10000.0, 0.9999, 0.01, instance.cities.len()*2);
+        },
+        "ils" => {
+            println!("ILS:");
+            solution = instance.ils(&random_solution);
+        }
+        _ => { println!("Nenhum metodo com esse nome!"); return },
+    }
+    let time_elapsed = start.elapsed();
+    println!("{:?}", solution);
+    println!("{}", instance.evaluate(&mut solution));
+    println!("Tempo de execucao: {:?}", time_elapsed);
 }
